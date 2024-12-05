93b4beaa4eff78cccd95fe21b983a2d8e086b71e
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2024-12-05 14:00:54 +0200

    feat!: add transforms module and update type handling in Firestore transforms

--- pydantic_firestore/__init__.py
+++ pydantic_firestore/__init__.py
@@ -2,15 +2,7 @@ from .path import FirestorePath
 from .reference import FirestoreCollection, FirestoreDocument
 from .snapshot import FirestoreSnapshot
 from .model import FirestoreModel, FirestoreDict
+from .transforms import *
 
 
 __version__ = "0.3.0"
-
-__all__ = [
-    "FirestorePath",
-    "FirestoreCollection",
-    "FirestoreDocument",
-    "FirestoreSnapshot",
-    "FirestoreModel",
-    "FirestoreDict",
-]
--- pydantic_firestore/transforms.py
+++ pydantic_firestore/transforms.py
@@ -1,4 +1,4 @@
-from typing import Annotated, Literal, Union, Any, Callable, TypeVar, Tuple
+from typing import Annotated, Literal, Union, Any, Callable, TypeVar, Tuple, TYPE_CHECKING
 from enum import StrEnum, auto, global_enum
 from functools import cache
 
@@ -64,16 +64,20 @@ class Sentinel(StrEnum):
     def to_firestore(self, *, value=None, **kwargs) -> object:
         return self.__class__.firestore_mapping()[self](value)
 
+    def __call__[T](self, value: T, /) -> "FirestoreSentinel[T]":
+        return (self, value)
 
-FIRESTORE_TIMESTAMP = Sentinel.FIRESTORE_TIMESTAMP
-FIRESTORE_DELETE = Sentinel.FIRESTORE_DELETE
 
-FIRESTORE_REMOVE = Sentinel.FIRESTORE_REMOVE
-FIRESTORE_UNION = Sentinel.FIRESTORE_UNION
+if TYPE_CHECKING:
+    FIRESTORE_TIMESTAMP = Sentinel.FIRESTORE_TIMESTAMP
+    FIRESTORE_DELETE = Sentinel.FIRESTORE_DELETE
 
-FIRESTORE_INCREMENT = Sentinel.FIRESTORE_INCREMENT
-FIRESTORE_MAXIMUM = Sentinel.FIRESTORE_MAXIMUM
-FIRESTORE_MINIMUM = Sentinel.FIRESTORE_MINIMUM
+    FIRESTORE_REMOVE = Sentinel.FIRESTORE_REMOVE
+    FIRESTORE_UNION = Sentinel.FIRESTORE_UNION
+
+    FIRESTORE_INCREMENT = Sentinel.FIRESTORE_INCREMENT
+    FIRESTORE_MAXIMUM = Sentinel.FIRESTORE_MAXIMUM
+    FIRESTORE_MINIMUM = Sentinel.FIRESTORE_MINIMUM
 
 FirestoreArray = Literal[Sentinel.FIRESTORE_REMOVE, Sentinel.FIRESTORE_UNION]
 FirestoreNumeric = Literal[
@@ -102,4 +106,4 @@ def _serialize_sentinel(value: Any, handler: Callable, info: ValidationInfo):
 sentinel_serializer = WrapSerializer(_serialize_sentinel, return_type=object)
 
 T = TypeVar("T")
-FirestoreSentinel = Annotated[Union[T, Tuple[Sentinel, T]], sentinel_serializer]
+FirestoreSentinel = Annotated[Union[T, Tuple[T]], sentinel_serializer]
